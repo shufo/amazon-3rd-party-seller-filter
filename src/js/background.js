b67585ba865fc78b1e4eeae49030a80017ee17e4
Shuhei Hayashibara <meikyowise@gmail.com>
2024-12-07 23:00:08 +0900

    fix: 🐛 #11 persist extension state by local storage

--- src/js/background.js
+++ src/js/background.js
@@ -5,60 +5,65 @@ import { providedByAmazon } from "./category.js";
 const status = new statusManager(true);
 const filterKey = "p_6";
 
+// On click event for the extension icon
 chrome.action.onClicked.addListener((tab) => {
-  status.changeStatus();
+  status.changeStatus(() => {
+    status.getStatus((result) => {
+      if (tab.url === undefined) {
+        return;
+      }
+      const url = new URL(tab.url);
+      const filter = queryParams[url.hostname];
 
-  if (status.getStatus()) {
-    chrome.action.setIcon({ path: "../../icon/favicon-32x32_on.png" });
-  } else {
-    chrome.action.setIcon({ path: "../../icon/favicon-32x32_off.png" });
-  }
-  if (tab.url === undefined) {
-    return;
-  }
-  const url = new URL(tab.url);
-  const filter = queryParams[url.hostname];
+      if (result.status) {
+        if (providedByAmazon(url)) {
+          return;
+        }
 
-  if (status.getStatus()) {
-   
-    if (providedByAmazon(url)) {
-      return;
-    }
+        if (url.searchParams.has("k") && !url.searchParams.has("rh")) {
+          url.searchParams.append("rh", `${filterKey}:${filter.value}`);
+          chrome.tabs.update(tab.id, { url: url.toString() });
+        }
 
-    if (url.searchParams.has("k") && !url.searchParams.has("rh")) {
-      url.searchParams.append("rh", `${filterKey}:${filter.value}`);
-      chrome.tabs.update(tab.id, { url: url.toString() });
-    }
-  } else {
-    if (url.searchParams.has("k") && url.searchParams.has("rh")) {
-      url.searchParams.delete("rh");
-      chrome.tabs.update(tab.id, { url: url.toString() });
-    }
-  }
+      } else {
+        if (url.searchParams.has("k") && url.searchParams.has("rh")) {
+          url.searchParams.delete("rh");
+          chrome.tabs.update(tab.id, { url: url.toString() });
+        }
+      }
+
+    });
+  });
 });
 
+// On update event for the tab
 chrome.tabs.onUpdated.addListener((tabId, tab) => {
-  if (status.getStatus()) {
-    chrome.action.setIcon({ path: "../../icon/favicon-32x32_on.png" });
-  } else {
-    chrome.action.setIcon({ path: "../../icon/favicon-32x32_off.png" });
-  }
-  if (tab.url === undefined) {
-    return;
-  }
-  const url = new URL(tab.url);
-  const filter = queryParams[url.hostname];
-
-  if (status.getStatus() && providedByAmazon(url)) {
+  if (tab.status !== "loading") {
     return;
   }
 
-  if (
-    status.getStatus() &&
-    url.searchParams.has("k") &&
-    !url.searchParams.has("rh")
-  ) {
-    url.searchParams.append("rh", `${filterKey}:${filter.value}`);
-    chrome.tabs.update(tabId, { url: url.toString() });
-  }
+  // Get the current status of the extension
+  status.getStatus((result) => {
+    if (result.status) {
+      statusManager.enableIcon();
+
+      if (tab.url === undefined) {
+        return;
+      }
+
+      const url = new URL(tab.url);
+      const filter = queryParams[url.hostname];
+
+      if (providedByAmazon(url)) {
+        return;
+      }
+
+      if (url.searchParams.has("k") && !url.searchParams.has("rh")) {
+        url.searchParams.append("rh", `${filterKey}:${filter.value}`);
+        chrome.tabs.update(tabId, { url: url.toString() });
+      }
+    } else {
+      statusManager.disableIcon();
+    }
+  });
 });
--- src/js/status.js
+++ src/js/status.js
@@ -1,14 +1,53 @@
 class statusManager {
   constructor(status) {
-    this.status = status;
+    // if already status set in local storage, then use it
+    // else initialize with true
+    chrome.storage.local.get("status").then((result) => {
+      if (result.status === undefined) {
+        this.initializeStatus(true);
+        statusManager.enableIcon();
+      }
+
+      if (result.status === true) {
+        statusManager.enableIcon();
+      }
+    });
   }
 
-  getStatus() {
-    return this.status;
+  // Initialize status in local storage
+  initializeStatus(status) {
+    chrome.storage.local.set({ "status": status }, () => {
+      console.log("Status initialized", status);
+    });
   }
 
-  changeStatus() {
-    this.status = !this.status;
+  // Get status from local storage
+  getStatus(callback) {
+    chrome.storage.local.get("status").then(callback);
+  }
+
+  // Change status in local storage
+  changeStatus(callback) {
+    this.getStatus((result) => {
+      const changesTo = !result.status;
+      chrome.storage.local.set({ "status": changesTo }, callback);
+
+      console.log("Status changed to", changesTo);
+
+      if (changesTo) {
+        statusManager.enableIcon();
+      } else {
+        statusManager.disableIcon();
+      }
+    });
+  }
+
+  static enableIcon() {
+    chrome.action.setIcon({ path: "../../icon/favicon-32x32_on.png" });
+  }
+
+  static disableIcon() {
+    chrome.action.setIcon({ path: "../../icon/favicon-32x32_off.png" });
   }
 }
 
